76d25c1b5d7dacfb50553726a90992589a61acfc
Ke-vin Chan <chan9367@gmail.com>
2020-06-05 16:22:36 -0500

    random button worked

--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -1,16 +1,94 @@
 import React, { Component } from "react";
+import SearchField from "./SearchField";
 import axios from "axios";
+
+
 
 class GifCard extends Component {
     constructor(props){
       super(props);
         this.state={
-        }
+          gifData: [],
+          randomData: "",
+          searchInput: "",
+          showRandom: false,
+        };
       }
   
   
+  handleInput = (event) => {
+    this.setState({searchInput: event.target.value});
+  }
+
+  handleSearch = () =>{
+    const searchInput = this.state.searchInput;
+    const API_KEY = process.env.REACT_APP_GIFY_API_KEY;
+    const url = "http://api.giphy.com/v1/gifs/search?q=" + searchInput + "&api_key=" + API_KEY;
+    axios
+      .get(url, { params: {key: API_KEY}})
+      .then((response) =>{
+       this.setState({gifData: response.data.data});
+       this.setState({showRandom : false});
+       console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleTrend = () =>{
+    const API_KEY = process.env.REACT_APP_GIFY_API_KEY;
+    const url = " http://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY;
+     axios
+      .get(url, { params: {key: API_KEY}})
+      .then((response) =>{
+       this.setState({gifData: response.data.data});
+       this.setState({showRandom : false})
+       console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleRandom = () =>{
+    const API_KEY = process.env.REACT_APP_GIFY_API_KEY;
+    const url = "http://api.giphy.com/v1/gifs/random?api_key=" + API_KEY;
+    axios
+      .get(url, { params: {key: API_KEY}})
+      .then((response) =>{
+       this.setState({randomData: response.data.data.images.downsized_large.url});
+       this.setState({showRandom : true});
+      console.log(this.state.randomData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   
   render(){
+     return(
+       <div>
+         <SearchField
+           value = {this.state.searchInput}
+           onChange = {this.handleInput}
+           onSearch = {this.handleSearch}
+           onTrend = {this.handleTrend}
+           onRandom = {this.handleRandom}
+         />
+        <div>
+          {this.state.gifData.map(data => {
+            if(this.state.showRandom!==true){return (
+              <div>
+                <img src={data.images.downsized_large.url} />
+              </div>
+            )}
+            
+          })}
+            <div><img src={this.state.randomData}/></div>
+        </div>
+       </div>
+     );
     }
   }
   
