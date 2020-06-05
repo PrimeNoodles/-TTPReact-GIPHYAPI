6b9e647399df6d7a63c37895fabe8a449a1b9188
unknown <klam9986@bths.edu>
2020-06-05 11:54:45 -0400

    Updated with initial framework

---
+++ src/Components/SearchField.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class SearchField extends Component {
+  constructor(props){
+    super(props);
+      this.state={
+      }
+    }
+
+
+
+render(){
+  }
+}
+
+export default SearchField;
