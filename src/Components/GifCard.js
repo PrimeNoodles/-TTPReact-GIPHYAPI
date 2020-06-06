d58d4fa760eebdcc8cfed7a86509d50d884b25c4
Ke-vin Chan <chan9367@gmail.com>
2020-06-05 21:39:15 -0500

    final commit

--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -11,7 +11,7 @@ class GifCard extends Component {
           gifData: [],
           randomData: "",
           searchInput: "",
-          showRandom: false,
+          showRandom: null,
         };
       }
   
@@ -19,7 +19,6 @@ class GifCard extends Component {
   handleInput = (event) => {
     this.setState({searchInput: event.target.value});
   }
-
   handleSearch = () =>{
     const searchInput = this.state.searchInput;
     const API_KEY = process.env.REACT_APP_GIFY_API_KEY;
@@ -29,13 +28,11 @@ class GifCard extends Component {
       .then((response) =>{
        this.setState({gifData: response.data.data});
        this.setState({showRandom : false});
-       console.log(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }
-
   handleTrend = () =>{
     const API_KEY = process.env.REACT_APP_GIFY_API_KEY;
     const url = " http://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY;
@@ -44,29 +41,27 @@ class GifCard extends Component {
       .then((response) =>{
        this.setState({gifData: response.data.data});
        this.setState({showRandom : false})
-       console.log(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }
-
   handleRandom = () =>{
     const API_KEY = process.env.REACT_APP_GIFY_API_KEY;
     const url = "http://api.giphy.com/v1/gifs/random?api_key=" + API_KEY;
     axios
       .get(url, { params: {key: API_KEY}})
       .then((response) =>{
-       this.setState({randomData: response.data.data.images.downsized_large.url});
-       this.setState({showRandom : true});
-      console.log(this.state.randomData);
+        this.setState({randomData: response.data.data.images.downsized_large.url});
+        this.setState({showRandom : true});
+       console.log(this.state.randomData);
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  
   render(){
+    let displayRandom = this.state.showrandom;
      return(
        <div>
          <SearchField
@@ -76,16 +71,20 @@ class GifCard extends Component {
            onTrend = {this.handleTrend}
            onRandom = {this.handleRandom}
          />
-        <div>
+        <div className="row row-cols-1 row-cols-md-3 mt-5">
           {this.state.gifData.map(data => {
             if(this.state.showRandom!==true){return (
-              <div>
-                <img src={data.images.downsized_large.url} />
-              </div>
+              <div  className ="col mb-4">
+               <div class="card border-danger cardSize" >
+                   <div><img src={data.images.downsized_large.url} class="card-img-top" alt="gif"></img>  </div>
+               <div class="card-body text-danger">
+                <h6 class="card-text ">{data.title}</h6>
+               </div>
+               </div>
+             </div>
             )}
-            
           })}
-            <div><img src={this.state.randomData}/></div>
+             <div><img src={this.state.randomData}/></div>
         </div>
        </div>
      );
